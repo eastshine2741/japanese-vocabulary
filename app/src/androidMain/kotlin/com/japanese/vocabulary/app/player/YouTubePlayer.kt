d34643e519dca62a0410119c917177b9a5d12a7f
eastshine2741 <eastshine@snu.ac.kr>
2026-03-08 20:25:20 +0900

    implement MV player with WebView

--- app/src/androidMain/kotlin/com/japanese/vocabulary/app/player/YouTubePlayer.kt
+++ app/src/androidMain/kotlin/com/japanese/vocabulary/app/player/YouTubePlayer.kt
@@ -1,24 +1,58 @@
 package com.japanese.vocabulary.app.player
 
+import android.webkit.CookieManager
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 @Composable
 actual fun YouTubePlayer(videoId: String, modifier: Modifier, onSecondChanged: (Float) -> Unit) {
+    val callbackRef = rememberUpdatedState(onSecondChanged)
+
     AndroidView(
         factory = { ctx ->
-            YouTubePlayerView(ctx).apply {
-                addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
-                    override fun onReady(youTubePlayer: com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer) {
-                        youTubePlayer.loadVideo(videoId, 0f)
+            WebView(ctx).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.mediaPlaybackRequiresUserGesture = false
+                CookieManager.getInstance().setAcceptCookie(true)
+
+                addJavascriptInterface(object {
+                    @JavascriptInterface
+                    fun onTimeUpdate(second: Double) {
+                        callbackRef.value(second.toFloat())
                     }
-                    override fun onCurrentSecond(youTubePlayer: com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer, second: Float) {
-                        onSecondChanged(second)
+                }, "Android")
+
+                webChromeClient = WebChromeClient()
+
+                webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        view?.evaluateJavascript(
+                            """
+                            (function() {
+                                function poll() {
+                                    var v = document.querySelector('video');
+                                    if (v) Android.onTimeUpdate(v.currentTime);
+                                    setTimeout(poll, 500);
+                                }
+                                poll();
+                            })();
+                            """.trimIndent(),
+                            null
+                        )
                     }
-                })
+                }
+
+                loadUrl(
+                    "https://www.youtube-nocookie.com/embed/$videoId" +
+                    "?autoplay=1&playsinline=1&rel=0"
+                )
             }
         },
         modifier = modifier
